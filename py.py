014f372df80fb16720a5cece124250449f505660
ALISHAMATHUR007 <mathuralisha7002@gmail.com>
2025-11-23 23:29:45 +0530

    Refactor file operations to write and read sample.txt

--- py.py
+++ py.py
@@ -1,9 +1,9 @@
-# f=open("sample.txt","w")
-# f.write("ine1:this is a simple text file.\n")
-# f.write("line2:it contains multiple lines.")
-# f.write('bye')
-# f.close()
-# print("work is done")
+f=open("sample.txt","w")
+f.write("ine1:this is a simple text file.\n")
+f.write("line2:it contains multiple lines.")
+f.write('bye')
+f.close()
+print("work is done")
 
 try:
     with open("sample.txt", "r") as file:
@@ -11,9 +11,5 @@ try:
         print(content)
 except FileNotFoundError:
     print("Error: The file 'sample.txt' does not exist.")
-# try:
-#     with open("s.txt", "r") as file:
-#         content = file.read()
-#         print(content)
-# except FileNotFoundError:
-#     print("Error: The file 'sample.txt' does not exist.")
+
+
